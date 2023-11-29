6668a3a510cc82364d92350366206084d6c341fc
kevin.perettimalaguti <kevin.peretti-malaguti@laplateforme.io>
2023-11-29 15:33:36 +0100

    Push calculatrice en 2 jours

--- calculatrice9000.py
+++ calculatrice9000.py
@@ -1,64 +1,197 @@
+# Importation de tkinter pour avoir une interface graphique
 from tkinter import *
 
+# Une classe qui contient toutes les fonctions pour calculer/afficher les chiffres/vérifier l'opération en cas d'erreur
+class Calculator():
+    def __init__(self): 
+        self.phase1 = 0 
+        self.phase2 = 0 
+        self.final = 0 
+        self.entry = StringVar() 
+        self.text = "" 
+        self.signe = ""        
+        
+    def init(self):  
+        self.phase1 = 0 
+        self.phase2 = 0 
+        self.final = 0 
+        self.text = "" 
+        self.signe = ""
+        
+    def afficher_Nb(self): # Afficher les nombres sur l'écran 
+        self.entry.set(self.text)
+        ecran.delete("1.0", END)
+        ecran.insert(END, self.text)
+
+    def operation(self): # Vérification de l'opération
+        try : 
+            if "+" in self.text:
+                self.Plus()
+            elif "-" in self.text:
+                self.Sous()
+            elif "/" in self.text:
+                self.Div()
+            elif "x" in self.text:
+                self.Mult()
+        except:
+            self.entry.set("ERROR")
+            self.init()
+
+
+    # Création des fonctions pour calculer: additionner, soustraire, mutiplier, diviser etc...    
+    def Plus(self): # Addition
+        nb = self.text.split("+")
+        self.phase1 = float(nb[0])
+        self.phase2 = float(nb[1])
+        self.final = self.phase1 + self.phase2
+        self.entry.set(str(self.final))
+        self.init()
+
+    def Sous(self): # Soustraction
+        nb = self.text.split("-")
+        self.phase1 = float(nb[0])
+        self.phase2 = float(nb[1])
+        self.final = self.phase1 - self.phase2
+        self.entry.set(str(self.final))
+        self.init()
+
+    def Div(self): # Division
+        nb = self.text.split("/")
+        self.phase1 = float(nb[0])
+        self.phase2 = float(nb[1])
+        self.final = self.phase1 / self.phase2
+        self.entry.set(str(self.final))
+        self.init()
+
+    def Mult(self): # Multiplication
+        nb = self.text.split("x")
+        self.phase1 = float(nb[0])
+        self.phase2 = float(nb[1])
+        self.final = self.phase1 * self.phase2
+        self.entry.set(str(self.final))
+        self.init()    
+
+# Fonctions pour activer tous les boutons
+def Bouton1 (): 
+    calculatrice.text += "1"
+    calculatrice.entry.set(calculatrice.text)    
+
+def Bouton2 (): 
+    calculatrice.text += "2"
+    calculatrice.entry.set(calculatrice.text)    
+
+def Bouton3 (): 
+    calculatrice.text += "3"
+    calculatrice.entry.set(calculatrice.text)    
+
+def Bouton4 ():
+    calculatrice.text += "4"
+    calculatrice.entry.set(calculatrice.text)    
+    
+def Bouton5 (): 
+    calculatrice.text += "5"
+    calculatrice.entry.set(calculatrice.text)    
+
+def Bouton6 ():
+    calculatrice.text += "6"
+    calculatrice.entry.set(calculatrice.text)    
+
+def Bouton7 (): 
+    calculatrice.text += "7"
+    calculatrice.entry.set(calculatrice.text)    
+
+def Bouton8 (): 
+    calculatrice.text += "8"
+    calculatrice.entry.set(calculatrice.text)
+    
+def Bouton9 (): 
+    calculatrice.text += "9"
+    calculatrice.entry.set(calculatrice.text) 
+
+def Bouton0 (): 
+    calculatrice.text += "0"
+    calculatrice.entry.set(calculatrice.text)
+
+def BoutonF(): 
+    calculatrice.text += "."
+    calculatrice.entry.set(calculatrice.text)    
+    
+def BoutonP (): 
+    calculatrice.text += "+"
+    calculatrice.entry.set(calculatrice.text)    
+
+def BoutonS (): 
+    calculatrice.text += "-"
+    calculatrice.entry.set(calculatrice.text)    
+
+def BoutonD ():
+    calculatrice.text += "/"
+    calculatrice.entry.set(calculatrice.text)    
+
+def BoutonM ():
+    calculatrice.text += "x"
+    calculatrice.entry.set(calculatrice.text)    
+
+def BoutonE ():
+    calculatrice.operation()    
+
+def BoutonC (): 
+    calculatrice.entry.set("")
+    calculatrice.init()  
+
+
+# La fenêtre principale graphique
 fenetre = Tk()
 fenetre.title("Calculatrice programmable")
-fenetre.geometry("410x500")
+fenetre.geometry("360x450")
+calculatrice = Calculator()
 
-# Écran principal
-ecran = Text(fenetre, width=20, height=2, font=("Arial", 26))
-ecran.grid(columnspan=5, padx=10, pady=10)
+# Ecran des calculs
+ecran= Entry(fenetre, width=50, textvariable=calculatrice.entry, relief=SUNKEN, bd=5)
+ecran.grid(columnspan=6, padx=20, pady=10)
 
-# Fonctions des boutons de la calculatrice (hors opérateur)
-class Boutons:
-    def __init__(self, valeur, row, column):
-        self.bouton = Button(fenetre, text=valeur, command=lambda: self.ajout_valeur(valeur), width=8, font=("Arial", 13), height=2)
-        self.bouton.grid(row=row, column=column, padx=6, pady=6)
+# Parametrer les boutons de ma calculette
+bouton1 = Button(fenetre, text="1", command=Bouton1, width=7, font=("Arial", 12), height=2) 
+bouton2 = Button(fenetre, text="2", command=Bouton2, width=7, font=("Arial", 12), height=2) 
+bouton3 = Button(fenetre, text="3", command=Bouton3, width=7, font=("Arial", 12), height=2)
+bouton4 = Button(fenetre, text="4", command=Bouton4, width=7, font=("Arial", 12), height=2)
+bouton5 = Button(fenetre, text="5", command=Bouton5, width=7, font=("Arial", 12), height=2) 
+bouton6 = Button(fenetre, text="6", command=Bouton6, width=7, font=("Arial", 12), height=2)
+bouton7 = Button(fenetre, text="7", command=Bouton7, width=7, font=("Arial", 12), height=2)
+bouton8 = Button(fenetre, text="8", command=Bouton8, width=7, font=("Arial", 12), height=2)
+bouton9 = Button(fenetre, text="9", command=Bouton9, width=7, font=("Arial", 12), height=2)
+boutonC = Button(fenetre, text="C", command=BoutonC, width=7, font=("Arial", 12), height=2) 
+bouton0 = Button(fenetre, text="0", command=Bouton0, width=7, font=("Arial", 12), height=2)
+boutonPourcentage = Button(fenetre, text="%", width=7, font=("Arial", 12), height=2)
+boutonParenthèseGauche = Button(fenetre, text="(", width=7, font=("Arial", 12), height=2)
+boutonParenthèseDroite = Button(fenetre, text=")", width=7, font=("Arial", 12), height=2)
+boutonPoint = Button(fenetre, text=".", command=BoutonF, width=7, font=("Arial", 12), height=2)
+boutonPlus = Button(fenetre, text="+", command=BoutonP, width=7, font=("Arial", 12), height=2) 
+boutonSous = Button(fenetre, text="-", command=BoutonS, width=7, font=("Arial", 12), height=2) 
+boutonDiv = Button(fenetre, text="/", command=BoutonD, width=7, font=("Arial", 12), height=2) 
+boutonMulti = Button(fenetre, text="x", command=BoutonM, width=7, font=("Arial", 12), height=2) 
+boutonEgal = Button(fenetre, text="=", command=BoutonE, width=7, font=("Arial", 12), height=2)
 
-    def ajout_valeur(self, valeur):
-        afficher = ecran.get("1.0", END)
-        ecran.delete("1.0", END)
-        ecran.insert(END, afficher.strip() + valeur)
+# Organisation des boutons avec la fonction grid
+bouton1.grid(row=1, column=1, padx=8, pady=8)
+bouton2.grid(row=1, column=2, padx=8, pady=8)
+bouton3.grid(row=1, column=3, padx=8, pady=8)
+bouton4.grid(row=2, column=1, padx=8, pady=8)
+bouton5.grid(row=2, column=2, padx=8, pady=8)
+bouton6.grid(row=2, column=3, padx=8, pady=8)
+bouton7.grid(row=3, column=1, padx=8, pady=8)
+bouton8.grid(row=3, column=2, padx=8, pady=8)
+bouton9.grid(row=3, column=3, padx=8, pady=8)
+bouton0.grid(row=4, column=2, padx=8, pady=8)
+boutonPoint.grid(row=4, column=3, padx=8, pady=8)
+boutonPlus.grid(row=1, column=4, padx=8, pady=8)
+boutonSous.grid(row=2, column=4, padx=8, pady=8)
+boutonDiv.grid(row=3, column=4, padx=8, pady=8)
+boutonMulti.grid(row=4, column=4, padx=8, pady=8)
+boutonEgal.grid(row=5, column=4, padx=8, pady=8)
+boutonC.grid(row=4, column=1, padx=8, pady=8)
+boutonPourcentage.grid(row=5, column=3, padx=8, pady=8)
+boutonParenthèseDroite.grid(row=5, column=2, padx=8, pady=8)
+boutonParenthèseGauche.grid(row=5, column=1, padx=8, pady=8)
 
-# Instanciation des boutons (chiffres)
-bouton1 = Boutons("1", 1, 1)
-bouton2 = Boutons("2", 1, 2)
-bouton3 = Boutons("3", 1, 3)
-bouton4 = Boutons("4", 2, 1)
-bouton5 = Boutons("5", 2, 2)
-bouton6 = Boutons("6", 2, 3)
-bouton7 = Boutons("7", 3, 1)
-bouton8 = Boutons("8", 3, 2)
-bouton9 = Boutons("9", 3, 3)
-bouton0 = Boutons("0", 4, 2)
-
-# Opérations
-class Operations:
-    def __init__(self, symbole, row, column):
-        self.operation = Button(fenetre, text=symbole, command=lambda: self.operation_clique(symbole), width=8, font=("Arial", 13), height=2)
-        self.operation.grid(row=row, column=column, padx=6, pady=6)
-
-    def operation_clique(self, symbole):
-        current = ecran.get("1.0", END)
-        if current and current[-1] not in ['+', '-', 'x', '/']:
-            ecran.insert(END, symbole) 
-
-# Fonction du bouton effacer
-def effacer_tout():        
-    ecran.delete("1.0", END)
-
-# Instanciation des boutons opérateurs
-operation_plus = Operations("+", 1, 4)
-operation_moins = Operations("-", 2, 4)
-operation_fois = Operations("x", 3, 4)
-operation_div = Operations("/", 4, 4)
-
-# Boutons égal, effacer, virgule, parenthèses
-pourcentage = Boutons("%", 4, 3)
-egal = Boutons("=", 5, 4)
-effacer = Button(fenetre, text="C", command=effacer_tout, width=8, font=("Arial", 13), height=2)
-effacer.grid(row=4, column=1, padx=6, pady=6)
-virgule = Boutons(".", 5, 2)
-parenthese_gauche = Boutons("(", 5, 1 )
-parenthese_droite = Boutons(")", 5, 3)
-
-fenetre.mainloop()
+fenetre.mainloop() 
